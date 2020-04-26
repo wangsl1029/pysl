d6cf12817188aebd2e4ad04a8db340a90046133a
wsongl mac <wangsl1029@yeah.net>
2020-04-26 23:49:28 +0800

    0426-2

--- wsl05token.py
+++ wsl05token.py
@@ -1,11 +1,15 @@
-import hashlib, time
+import hashlib
+import random
+import string
+import time
+
 
 def token(username):
     """传入用户名，生成token"""
-    API_SECRET = "Wsl19911991lsW"
+    rand_secret = ''.join(random.sample(string.ascii_letters + string.digits, 8))
     time_stamp = str(time.time())
     token = hashlib.md5()
-    token.update((API_SECRET + time_stamp).encode("utf-8"))
+    token.update((rand_secret + time_stamp + username).encode("utf-8"))
     return token.hexdigest()
 
 
